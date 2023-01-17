5b0d2f93640cbb757de653d1ab78ba9e8b079109
Arif khan <arifkhanshanto123@gmail.com>
2023-01-17 14:17:38 +0600

    data changed 17 Ja 2022

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ function airport(params) {
             }
         })
     }
+    extra_obj = {"iata_code":"ZVJ","time_zone_id":"Asia/Dubai","name":"EK Bus Station Airport","city_code":"AUH","country_id":"AE","location":"POINT (124.432972 54.651138)","elevation":62,"url":"","icao":"","city":"Abu Dhabi","county":"United Arab Emirates","municipality":"Abu Dhabi,Dubai","id":9766}
+    arr.push(extra_obj);
     return arr;
+
+    
 }
 console.log(airport("daC"));
 module.exports = airport
